ed3d7d70cd823e4ee56ce3d95465aa87531e6c0b
Kristian Quirapas <kmquirapas@gmail.com>
2024-12-22 05:00:14 +0800

    feat: Configured Ping behavior

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use futures::prelude::*;
+use libp2p::swarm::SwarmEvent;
+use libp2p::{ping, Multiaddr};
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let ping_config = ping::Config::default()
+        .with_timeout(Duration::from_secs(5))
+        .with_interval(Duration::from_secs(1));
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            libp2p::tcp::Config::default(),
+            libp2p::tls::Config::new,
+            libp2p::yamux::Config::default,
+        )?
+        .with_behaviour(|_| ping::Behaviour::new(ping_config))?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX))) // Allows us to observe pings indefinitely.
+        .build();
+
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Dial the peer identified by the multi-address given as the second
+    // command-line argument, if any.
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {addr}")
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
+            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+
+            _ => {}
+        }
+    }
+
+    // Ok(())
 }
